d57620a917321cff508d578ef7ef001d1cf7df89
Bhargav Naik <bhargav.naik@flipkart.com>
2018-01-28 00:34:08 +0530

    zk kafka hbase

---
+++ kafka/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+if [ -z $KAFKA_JMX_OPTS ]; then
+    KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true"
+    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
+    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.ssl=false"
+    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.rmi.port=9977"
+    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Djava.rmi.server.hostname=kafka "
+    export KAFKA_JMX_OPTS
+fi
+
+echo "Starting kafka"
+exec /kafka/bin/kafka-server-start.sh /kafka/config/server.properties
